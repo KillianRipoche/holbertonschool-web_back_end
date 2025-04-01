ffbef98cd9d3d51b54ca682899b049dbfefcb54e
KillianRipoche <killian.ripoche@gmail.com>
2025-04-01 11:43:28 +0200

    all tasks

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((result) => {
+      // Pour chaque résultat, on renvoie un objet avec "status" et "value"
+      if (result.status === 'fulfilled') {
+        return { status: result.status, value: result.value };
+      }
+      return { status: result.status, value: result.reason };
+    }));
+}
