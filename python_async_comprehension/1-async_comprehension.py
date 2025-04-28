4bcaa75ad1b85864d8f224c10a6e91ad87ee165e
KillianRipoche <killian.ripoche@gmail.com>
2025-04-28 11:06:19 +0200

    encore une fois

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
-"""
-Collect 10 random floats using async comprehensions.
-"""
-from typing import List
+"""Module containing a coroutine that
+collects random numbers asynchronously."""
 
-# Dynamic import of the async_generator from file 0-async_generator.py
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """
-    Return a list with async generator
-    """
-    return [number async for number in async_generator()]
+    """ 10 random numbers"""
+    return [i async for i in async_generator()]
