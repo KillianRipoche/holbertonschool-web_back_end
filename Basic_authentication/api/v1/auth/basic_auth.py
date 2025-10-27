cabffc78085027ffd3f4f08d19d0eaa50739e21d
KillianRipoche <killian.ripoche@gmail.com>
2025-10-27 10:23:41 +0100

    task 7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,23 @@ class BasicAuth(Auth):
     """
     BasicAuth class that inherits from Auth
     """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Extracts the Base64
+        Args:
+            authorization_header: the Authorization header string
+        Returns:
+            The Base64 part after 'Basic '
+        """
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header[6:]
