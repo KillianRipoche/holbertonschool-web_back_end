e96855d442b90b4ee31e7cd68053e49fd80ec552
KillianRipoche <killian.ripoche@gmail.com>
2025-12-01 10:16:40 +0100

    Task1

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Module for Redis cache operations
+"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """
+    Cache class for storing data in Redis
+    """
+
+    def __init__(self):
+        """
+        Initialize Redis client and flush the database
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Store data in Redis with a random key
+
+        Args:
+            data: The data to store (can be str, bytes, int, or float)
+
+        Returns:
+            str: The randomly generated key used to store the data
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
