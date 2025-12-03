2ecf0e0fcd9e98e3c1b6b33c374cbd4b6fe6ae12
KillianRipoche <killian.ripoche@gmail.com>
2025-12-03 10:50:31 +0100

    Fix my task

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,6 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-SELECT band_name,
-       IFNULL(split, 2022) - formed AS lifespan
+SELECT band_name, (IFNULL(split, 2024) - formed) as lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+WHERE style LIKE "%Glam rock%"
+ORDER BY (IFNULL(split, 2024) - formed) DESC;
